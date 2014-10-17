a7110fb6b16db84442fb29e2b91187a7f68fcda6
David Fernando Redondo Durand <alu0100851700@ull.edu.es>
2014-10-17 16:02:39 +0100

    Creación de la clase complex.rb y eliminación de archivos sobrantes

---
+++ lib/complex.rb
@@ -0,0 +1,44 @@
+class Complex
+  attr_reader :x, :y
+  def initialize(x, y)
+    @x, @y = x, y
+  end
+  def to_s
+    "(#{@x},#{@y})"
+  end
+  
+  #Suma
+  def + (other)	
+    Complex.new(@x + other.x, @y + other.y)
+  end
+  
+  #Resta
+  def - (other)	
+    Complex.new(@x - other.x, @y + other.y)
+  end
+  
+  def *(other)	
+    
+    #Multiplicación
+    if other.is_a?(Complex)
+    Complex.new((@x * other.x) - (@y * other.y), (@x * other.y) - (@y * other.x))
+    
+    #Producto escalar
+    if other.is_a?(Fixnum)
+    Complex.new(@x * value, @y * value)
+  end
+  
+  #División
+  def / (other)	
+    Complex.new(((@x * other.x) + (@y * other.y)) / ( (other.x*other.x) + (other.y*other.y)) , (@y * other.x) - (@x * other.y) / ( (other.x*other.x) + (other.y*other.y))
+  end
+  
+ 
+  
+  def -@	#Inverso
+    Complex.new(@x * -1, @y * -1)
+  end
+
+  def 
+
+end
